1cbf60df9d96e6c0705cae10394077001ee4477b
Roman <dev@rman.dev>
2026-03-22 22:08:42 +0000

    Update get-matches.lib.test.ts

--- tests/get-matches.lib.test.ts
+++ tests/get-matches.lib.test.ts
@@ -3,17 +3,19 @@ import { getMatches } from "#lib/get-matches";
 
 describe("getMatches", () => {
   it("should correctly find matches", () => {
-    expect(getMatches("hello world", "world")).toEqual([6]);
+    expect(getMatches("hello world", "world")).toEqual([{ start: 6, end: 11 }]);
   });
 
   it("should be case insensitive by default", () => {
-    expect(getMatches("Hello World", "hello")).toEqual([0]);
-    expect(getMatches("Hello World", "WORLD")).toEqual([6]);
-    expect(getMatches("HELLO world", "World")).toEqual([6]);
+    expect(getMatches("Hello World", "hello")).toEqual([{ start: 0, end: 5 }]);
+    expect(getMatches("Hello World", "WORLD")).toEqual([{ start: 6, end: 11 }]);
+    expect(getMatches("HELLO world", "World")).toEqual([{ start: 6, end: 11 }]);
   });
 
   it("should be case sensitive when caseSensitive is true", () => {
-    expect(getMatches("Hello World", "Hello", true)).toEqual([0]);
+    expect(getMatches("Hello World", "Hello", true)).toEqual([
+      { start: 0, end: 5 },
+    ]);
     expect(getMatches("Hello World", "hello", true)).toEqual([]);
     expect(getMatches("Hello World", "HELLO", true)).toEqual([]);
   });
@@ -29,8 +31,17 @@ describe("getMatches", () => {
   });
 
   it("should find multiple matches", () => {
-    expect(getMatches("hello hello HELLO", "hello")).toEqual([0, 6, 12]);
-    expect(getMatches("hello hello HELLO", "hello", true)).toEqual([0, 6]);
-    expect(getMatches("hello hello HELLO", "HELLO", true)).toEqual([12]);
+    expect(getMatches("hello hello HELLO", "hello")).toEqual([
+      { start: 0, end: 5 },
+      { start: 6, end: 11 },
+      { start: 12, end: 17 },
+    ]);
+    expect(getMatches("hello hello HELLO", "hello", true)).toEqual([
+      { start: 0, end: 5 },
+      { start: 6, end: 11 },
+    ]);
+    expect(getMatches("hello hello HELLO", "HELLO", true)).toEqual([
+      { start: 12, end: 17 },
+    ]);
   });
 });
